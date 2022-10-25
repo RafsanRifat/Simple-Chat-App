03ebd6c9282eb7b2d188166c60f244f0389f0e8f
RafsanRifat <rafsan.rifat255@gmail.com>
2022-10-25 18:08:24 +0600

    consumers added

--- chat/urls.py
+++ chat/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import lobby
+from .views import lobby, room
 
 urlpatterns = [
-    path('', lobby, name='lobby')
+    path('', lobby, name='lobby'),
+    path('<str:room_name>/', room, name='room'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -5,3 +5,9 @@ from django.shortcuts import render
 
 def lobby(request):
     return render(request, 'chat/lobby.html')
+
+def room(request, room_name):
+    username = request.GET.get('username', 'Anonymous')
+    # messages = Message.objects.filter(room=room_name)[0:25]
+
+    return render(request, 'chat/room.html', {'room_name': room_name, 'username': username,})
