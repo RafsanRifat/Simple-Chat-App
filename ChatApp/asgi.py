bb2c7e09f633ac6e5ac01fd185f6f1b474881c59
RafsanRifat <rafsan.rifat255@gmail.com>
2022-10-25 18:13:55 +0600

    asgi file edited

--- ChatApp/asgi.py
+++ ChatApp/asgi.py
@@ -1,21 +1,42 @@
-"""
-ASGI config for ChatApp project.
+# """
+# ASGI config for ChatApp project.
+#
+# It exposes the ASGI callable as a module-level variable named ``application``.
+#
+# For more information on this file, see
+# https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+# """
+#
+# import os
+# from channels.routing import ProtocolTypeRouter
+# from django.core.asgi import get_asgi_application
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChatApp.settings')
+#
+# django_asgi_app = get_asgi_application()
+#
+# application = ProtocolTypeRouter({
+#     "http": django_asgi_app,
+#     # Just HTTP for now. (We can add other protocols later.)
+# })
 
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
-"""
 
 import os
-from channels.routing import ProtocolTypeRouter
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
 from django.core.asgi import get_asgi_application
+
+import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChatApp.settings')
 
-django_asgi_app = get_asgi_application()
-
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    # Just HTTP for now. (We can add other protocols later.)
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    )
 })
